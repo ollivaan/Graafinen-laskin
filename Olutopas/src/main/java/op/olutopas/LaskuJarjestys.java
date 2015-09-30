eba7d5e656a6e359e026f16b5fdcb932cf985cc7
Väänänen Ollipekka <ollivaan@da5-cs-b221-18.helsinki.fi>
2015-09-30 15:31:21 +0300

    laskujarjestys lisays

--- Olutopas/src/main/java/op/olutopas/LaskuJarjestys.java
+++ Olutopas/src/main/java/op/olutopas/LaskuJarjestys.java
@@ -1,17 +1,26 @@
-
-
 package op.olutopas;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  *
  * @author ollivaan
  */
 public class LaskuJarjestys {
-    
+
+    public LaskukaavaIkkuna laskukaavaikkuna;
+    public JButton button;
+    public JTextArea textArea;
+    public JTextField input;
+
     public LaskuJarjestys() {
-        
+
     }
+
     public void laske() {
-        
+        String merkit [] = {"+","-","*"}; //jne
+//        String lause = input.getText();
     }
 }
