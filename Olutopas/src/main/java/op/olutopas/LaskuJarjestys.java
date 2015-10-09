0e5b7c26bd5724af07100bddd89b36f21c1ab89b
Ollipekka Väänänen <ollipekka.vaananen@helsinki.fi>
2015-10-09 21:52:14 +0300

    laskujärjestys, mutta kesken

--- Olutopas/src/main/java/op/olutopas/LaskuJarjestys.java
+++ Olutopas/src/main/java/op/olutopas/LaskuJarjestys.java
@@ -1,13 +1,11 @@
 package op.olutopas;
 
-import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.util.Stack;
-//KESKEN!
+//KESKEN! Jako ei toimi kunnolla 9> ei toimi. ja logiikka... noh.
 
-<<<<<<< HEAD
 public class LaskuJarjestys {
 
     public LaskukaavaIkkuna laskukaavaikkuna;
@@ -263,58 +261,6 @@ public class LaskuJarjestys {
 
     }
 
-=======
-/**
-*
-* @author ollivaan
-*/
-public class LaskuJarjestys {
-/**
- * Luokka lukee laskun ja määrittää laskujärjestyksen
- * ja välittää laskut laskitoimitukset luokalle
- *
- */
-
-public LaskukaavaIkkuna laskukaavaikkuna;
-public JButton button;
-public JTextArea textArea;
-public JTextField input;
-public Laskutoimitukset laskutoimitukset;
-
-/**
- * Metodi LaskuJarjestys luo laskujärjestyksen
- *
- */
-public LaskuJarjestys(JTextArea textArea) {
-    this.textArea=textArea;
-}
-// LUON BINAARIPUULLA TÄMÄN LUOKAN TÄMÄ LUOKKA ON KESKEN!
-public void laske(String lauseke) {
-    String merkit [] = {"*","/"}; 
-    char kirjain;
-//        this.textArea.setText(lauseke);
-    char[] lausekechar = lauseke.toCharArray(); 
-//        System.out.println(lausekechar.length);
-   for(int pituus = lausekechar.length; pituus >= 0; pituus--) {
-
-       for(int merkki = merkit.length-1; merkki>=0; merkki--) {
-           if(lauseke.startsWith(merkit[merkki]) || lauseke.endsWith(merkit[merkki])) {              
-               this.textArea.setText("Lasku ei voi alkaa/loppua kerto tai jako merkillä");
-               return;
-           }              
-       }
-       if(lausekechar[pituus]=='+') {
-//             double arvo = Double.parseDouble(lausekechar[pituus-1]);
-           this.laskutoimitukset.plussaa(lausekechar[pituus-1], lausekechar[pituus+1]);
-
-       }
-         this.textArea.setText(lauseke);     
-
-  }
-
-            }
-
->>>>>>> 9287994231c943bc68c15998cb88bc1a35335f5a
 }
     class NodeDouble {
     double data;
